ae661af2c6e943b10e44e890631d9328636a725a
halaszk <halaszk@deathlord.eu>
2013-10-07 13:14:16 +0200

    Alpha10

--- sbin/perseus.sh
+++ sbin/perseus.sh
@@ -4,15 +4,15 @@ mount -o remount,rw /system
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
 
 if [ ! -f /system/xbin/su ]; then
-  mv /res/su /system/xbin/su
+	mv /res/su /system/xbin/su
 fi
 
 chown 0.0 /system/xbin/su
 chmod 06755 /system/xbin/su
-symlink /system/xbin/su /system/bin/su
+ln -s /system/xbin/su /system/bin/su
 
 if [ ! -f /system/app/Superuser.apk ]; then
-  mv /res/Superuser.apk /system/app/Superuser.apk
+	mv /res/Superuser.apk /system/app/Superuser.apk
 fi
 
 chown 0.0 /system/app/Superuser.apk
@@ -38,20 +38,33 @@ echo 480 > /sys/devices/platform/pvrsrvkm.0/sgx_dvfs_max_lock
 echo 50 > /sys/class/devfreq/exynos5-busfreq-mif/polling_interval
 echo 70 > /sys/class/devfreq/exynos5-busfreq-mif/time_in_state/upthreshold
 
-if [ ! -f /system/app/STweaks.apk ]; then
-  cat /res/STweaks.apk > /system/app/STweaks.apk
-  chown 0.0 /system/app/STweaks.apk
-  chmod 644 /system/app/STweaks.apk
+copySynapse() {
+	cat /res/synapse/Synapse.apk > /system/app/Synapse.apk
+	chown 0.0 /system/app/Synapse.apk
+	chmod 644 /system/app/Synapse.apk
+}
+
+if [ ! -f /data/nosynapse ]; then
+	if [  ! -f /system/app/Synapse.apk ]; then
+		copySynapse
+	else
+		R5=$(md5sum < /res/synapse/Synapse.apk | tr -d ' -')
+		S5=$(md5sum < /system/app/Synapse.apk | tr -d ' -')
+		if [ $R5 != $S5 ]; then
+			copySynapse
+		fi
+	fi
 fi
 
 mkdir -p /mnt/ntfs
 chmod 777 /mnt/ntfs
 mount -o mode=0777,gid=1000 -t tmpfs tmpfs /mnt/ntfs
 
-/res/uci.sh apply
+ln -s /res/synapse/uci /sbin/uci
+/sbin/uci
 
 if [ -d /system/etc/init.d ]; then
-  /sbin/busybox run-parts /system/etc/init.d
+	/sbin/busybox run-parts /system/etc/init.d
 fi;
 
 /sbin/busybox mount -t rootfs -o remount,ro rootfs
